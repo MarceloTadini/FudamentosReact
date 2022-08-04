4acccc18106d80fd7422edacb03b93d069c02c20
Marcelo Tadini <tadini.marcelo53@gmail.com>
2022-08-03 21:12:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Post} from "./Post"
+
+export function App() {
+
+  return (
+    <div>
+      <Post 
+        author="Marcelo" 
+        content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Volup"
+      />
+    </div>
+    
+  )
+}
+
+
